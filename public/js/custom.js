89dd3a24b2c65125d5797e15b94f72c32faafbf5
Wendly Saintil <wendly@servone.wspecs.com>
2017-09-30 01:14:44 +0300

    auto update 1506723283

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,7 +2,7 @@ $('#query').keyup(function(e){
   if(e.keyCode == 13) {
     const query = $(this).val();
     if (query) {
-      window.location.href = 'sdah/search/' + encodeURIComponent(query);
+      window.location.href = 'search/' + encodeURIComponent(query);
     }
   }
 });
