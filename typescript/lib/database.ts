8a10a64decd547953c79000444b72a23ae05abbe
Wendly Saintil <wendly@servone.wspecs.com>
2017-09-29 23:36:29 +0300

    auto update 1506717386

--- typescript/lib/database.ts
+++ typescript/lib/database.ts
@@ -10,7 +10,7 @@ database.Promise = global.Promise;
 const host = `127.0.0.1:27017/${config.col}`;
 
 let url = `mongodb://${host}`;
-if (config.col_auth  && !config.skip_auth) {
+if (config.col_auth  && config.col_auth) {
   url = `mongodb://${config.col}:${config.col_auth}@${host}`;
 }
 if (config.col) {
