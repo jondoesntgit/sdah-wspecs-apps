0e222d6497a8e8579c47bedbcf92242d78dc43b9
Wendly Saintil <wendly@servone.wspecs.com>
2017-09-29 22:03:46 +0300

    auto update 1506711823

--- data/denorm.js
+++ data/denorm.js
@@ -1,4 +1,4 @@
-const Hymn = require('./../../demodels');
+const Hymn = require('./../../dest/models/sdah');
 
 let fs = require('fs');
 let hymns = JSON.parse(fs.readFileSync('data/json/hymns.json'));
